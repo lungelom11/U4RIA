b7eabf5d622042059b6f5cef930f0e7ebef591fb
lungelom11 <lungelomkhwemte11@gmail.com>
2024-08-15 00:06:22 +0200

    added basename

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
-import HomePage from './pages/HomePage/HomePage'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import './styles.css'
-import MainLayout from './layout/MainLayout'
-import OurStory from './pages/OurStoryPage/OurStory'
-import OurMission from './pages/OurMissionPage/OurMission'
-import OurProduct from './pages/OurProductPage/OurProduct'
+import HomePage from './pages/HomePage/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './styles.css';
+import MainLayout from './layout/MainLayout';
+import OurStory from './pages/OurStoryPage/OurStory';
+import OurMission from './pages/OurMissionPage/OurMission';
+import OurProduct from './pages/OurProductPage/OurProduct';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/our-story" element={<OurStory />} />
-            <Route path="/our-mission" element={<OurMission />} />
-            <Route path="/our-products" element={<OurProduct />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter basename="/U4RIA">
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="our-story" element={<OurStory />} />
+          <Route path="our-mission" element={<OurMission />} />
+          <Route path="our-products" element={<OurProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
